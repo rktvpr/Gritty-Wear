c6ade4c158afb8de07c1941b155d5bd4b167d96f
Rahsan Lewis <rahsanlewis@gmail.com>
2022-12-15 18:56:52 -0500

    Working on server routes

--- views/server.js
+++ views/server.js
@@ -1,20 +1,41 @@
-const path = require('path');
-const express = require('express');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-const app = express();
-const PORT = process.env.PORT || 3001;
-const sequelize = require('./config/connection');
+// import the required external libraries
+const express = require("express")
+const { json } = require("body-parser")
+const addToCart = require("./store")
+const getCartItems = require("./store")
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// initialize the express application
+const app = express()
+app.use(json())
+// this serves all files in the `public` directory
+// and can be used to serve our HTML website
+app.use(express.static("public"))
+// this renders all "ejs" pages within the `views` directory
+app.set("view engine", "ejs")
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+const port = 3000
 
-app.use(require('./controllers/dish-routes'));
+// our application can have multiple users, but for now, let's assume there's a single user
+// who has this user name
+const userName = "sample user"
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+// const path = require('path');
+// const express = require('express');
+// const exphbs = require('express-handlebars');
+// const hbs = exphbs.create({});
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+// const sequelize = require('./config/connection');
+
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(require('./controllers/dish-routes'));
+
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => console.log('Now listening'));
+// });
