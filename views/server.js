f1037d828cb3d81a9a29887d172ffd8a702d64e0
Rahsan Lewis <rahsanlewis@gmail.com>
2022-12-15 21:03:10 -0500

    changes to server

--- views/server.js
+++ views/server.js
@@ -1,27 +1,21 @@
 const express = require("express")
 const app = express()
+const path = require('path');
+const exphbs = require('express-handlebars');
 const { json } = require("body-parser")
 const port = 3000
 
 const { addToCart, getCartItems } = require("./store")
 
 app.use(json())
-// this serves all files in the `public` directory
-// and can be used to serve our HTML website
 app.use(express.static("public"))
-// this renders all "ejs" pages within the `views` directory
-app.set("view engine", "ejs")
 
-// start an express server on the given port
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`)
 })
 
-// const path = require('path');
-// const express = require('express');
-// const exphbs = require('express-handlebars');
+
 // const hbs = exphbs.create({});
-// const app = express();
 // const PORT = process.env.PORT || 3001;
 // const sequelize = require('./config/connection');
 
