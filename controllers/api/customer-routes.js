2a9f374ee0473d6200ca8a58173a1efdf82f2796
Rick welte <rktvpr@gmail.com>
2022-12-21 22:43:03 -0500

    finished customer routes

--- controllers/api/customer-routes.js
+++ controllers/api/customer-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { getCustomers, createCustomer, getCustomer, updateCustomer, deleteCustomer } = require("../../repository/customerRepository")
-
+const { getCustomerOrders, deleteCustomerOrder } = require("../../repository/customerOrderRepository")
+const { deleteOrderItemByCustomerOrder } = require("../../repository/orderItemRepository")
 
 router.get('/', async (req, res) => {
   try {
@@ -60,10 +61,35 @@ router.delete('/:id', async (req, res) => {
       res.status(404).json({ message: 'No customer found with this id!' });
       return;
     }
+    let orders = await getCustomerOrders(req.params.id);
+    orders = orders.map(order => order.get({plain: true})); 
+    
+    const orderProcess = (orders) => {
+      // another section to take in input... and create a dynamic promise
+    
+      return new Promise( (resolve, reject) =>  {
+      
+          orders.forEach(async (order, index) => {
+            let customerOrderId = order.id;
+            await deleteOrderItemByCustomerOrder(customerOrderId);
+            await deleteCustomerOrder(customerOrderId);
+            if (index == orders.length - 1){
+              resolve({});
+            }
+          })
+          // good
+          // console.log(results);
+          if (orders.length == 0){
+            resolve({})
+          }
+      });
+    };
+    await orderProcess(orders);
     await deleteCustomer(req.params.id);
     res.status(204).send();
 
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
