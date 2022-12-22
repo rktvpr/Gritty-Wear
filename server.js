68b040eb122a79da8e792a3dd32e3a4bf61d81b6
Anne <aepanagotopulos@gmail.com>
2022-12-22 18:15:43 -0500

    session

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const path = require('path')
 const exphbs = require('express-handlebars')
 const app = express();
 const PORT = process.env.PORT || 3000;
-const hbs = exphbs.create()
+
 
 
 // Stripe test secret API key.
@@ -60,6 +60,29 @@ const sess = {
 };
 
 app.use(session(sess));
+
+const sess = {
+  secret: 'Super secret secret',
+  resave: false,
+  saveUninitialized: true,
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
 
 app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
